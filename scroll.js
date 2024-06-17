c90a8ab83c55149f119dc4fb28463e17eaee90af
LettyciaDev <lettycia.vitoriafranca@gmail.com>
2024-06-17 14:07:16 -0300

    código

---
+++ scroll.js
@@ -0,0 +1,52 @@
+
+window.exibir = ScrollReveal({reset:true})
+
+//TOPO DO SITE
+exibir.reveal('.effect-head',
+{
+    duration: 2000,
+    distance: '90px'
+
+})
+exibir.reveal('.about-ttl',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.social-media',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.about-txt',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.skill-title',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.skills-cards',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.projects-title',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
+exibir.reveal('.container',
+{
+    duration: 1500,
+    distance: '90px'
+
+})
