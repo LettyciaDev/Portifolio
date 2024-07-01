df73517196e661e0128c72f47814a64fd2a97625
LettyciaDev <lettycia.vitoriafranca@gmail.com>
2024-07-01 13:49:37 -0300

    atualizado 2.3

--- scroll.js
+++ scroll.js
@@ -5,60 +5,60 @@ window.exibir = ScrollReveal({reset:true})
 exibir.reveal('.effect-head',
 {
     duration: 2000,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.about-ttl',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.social-media',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.about-txt',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.skill-title',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.skills-cards',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.projects-title',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.container',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
 
 })
 exibir.reveal('.contact-title',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
     
 })
 exibir.reveal('.forms',
 {
     duration: 1500,
-    distance: '90px'
+    distance: '10%'
         
 })
